ee5169b0e66269e9c29199f93daaf09f7e88fa07
Brandon Haley <kyle7286@gmail.com>
2021-01-14 12:30:53 -0500

    added more audio links

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -141,7 +141,7 @@ function donePage() {
     var finalScore = document.getElementById("finalScore");
     var btnSubmit = document.getElementById("btnSubmit");
     var txtInitials = document.getElementById("txtbox_Initials");
-    var auSmallItem = new Audio('./assets/audio/OOT_Fanfare_SmallItem.mp3')
+    var auSmallItem = new Audio('http://noproblo.dayjo.org/ZeldaSounds/OOT/OOT_Fanfare_SmallItem.wav')
 
     auSmallItem.play();
 
